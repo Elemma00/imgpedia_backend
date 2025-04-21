78d6b87e85f4b685dfe2747a36c53daf06ef1777
Elemma00 <faundez76@gmail.com>
2025-04-21 13:46:46 -0400

    Update WebConfig.java

--- src/main/java/com/imgpedia/imgpedia_backend/configuration/WebConfig.java
+++ src/main/java/com/imgpedia/imgpedia_backend/configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("*")
+                .allowedMethods("POST");
             }
         };
     }
